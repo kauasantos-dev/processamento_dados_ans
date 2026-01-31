d0bc63d02191669aea8fb680947af3632f58352d
Kaua Santos <kavillykaua@gmail.com>
2026-01-31 01:51:45 -0300

    feat: adiciona função de extração de arquivo ZIP
    - Define diretório padrão para salvar o conteúdo extraído do ZIP
    - Importa requests, os, io e ZipFile para acessar, extrair e salvar o conteúdo do arquivo ZIP
    - Utiliza try/except para tratar erros de requisições HTTP

---
+++ extrair_arquivos.py
@@ -0,0 +1,20 @@
+import os
+import io
+import requests
+from zipfile import ZipFile
+
+def extrair_arquivo_zip(url_zip, pasta_destino='trimestres_2025'):
+    os.makedirs(pasta_destino, exist_ok=True)
+
+    try:
+        arquivo_zip = requests.get(url_zip, timeout=20)
+        arquivo_zip.raise_for_status()
+    except requests.RequestException:
+        return False
+    
+    with ZipFile(io.BytesIO(arquivo_zip.content)) as file:
+        file.extractall(pasta_destino)
+
+    return True
+
+print(extrair_arquivo_zip("https://dadosabertos.ans.gov.br/FTP/PDA/demonstracoes_contabeis/2025/2T2025.zip"))
