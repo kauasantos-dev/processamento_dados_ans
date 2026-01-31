299cb5df6b6807ebef179d0c692aea261126d9df
Kaua Santos <kavillykaua@gmail.com>
2026-01-31 16:57:41 -0300

    feat: adiciona função que verifica se um conteúdo específico existe em um arquivo
    - Recebe o caminho do arquivo e o conteúdo específico
    - Passa os dados do arquivo incrementalmente para melhor performance
    - Transforma o DataFrame em texto puro e verifica se o conteúdo específico está presente no texto
    - Retorna valor booleano de acordo com o resultado da verificação

--- extrair_arquivos.py
+++ extrair_arquivos.py
@@ -1,6 +1,7 @@
 import os
 import io
 import requests
+import pandas as pd
 from zipfile import ZipFile
 
 def extrair_arquivo_zip(url_zip, pasta_destino='trimestres_2025'):
@@ -16,3 +17,10 @@ def extrair_arquivo_zip(url_zip, pasta_destino='trimestres_2025'):
         file.extractall(pasta_destino)
 
     return True
+
+def verificar_dado_especifico(caminho_arquivo, dado_especifico):
+    for chunk in pd.read_csv(caminho_arquivo, delimiter=';', chunksize=10000):
+        dado_encontrado = chunk.astype(str).apply(lambda x: x.str.contains(str(dado_especifico), case=False, na=False)).any().any()
+        if dado_encontrado:
+            return True
+    return False 
